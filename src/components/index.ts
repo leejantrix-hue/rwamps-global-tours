fd5390717826d30f7f45873c7505faa6033f4a86
leejantrix-hue <leejantrix@gmail.com>
2026-06-03 22:19:24 +0300

    feat(components): add barrel export

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { Container } from './Container/Container';
+export { Button } from './Button/Button';
+export { Card } from './Card/Card';
+export { SectionHeading } from './SectionHeading/SectionHeading';
